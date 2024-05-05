8ec14f82d7baf915843e7869ed63624fe1e5d3dd
Natalia Zaitseva <zay-natalka04@yandex.ru>
2024-05-05 23:55:27 +0300

    table-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Table, Button } from 'antd';
+import React from 'react';
+import { Table } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
 
 interface DataType {
@@ -10,8 +10,6 @@ interface DataType {
   email: string;
   tags: string[];
 }
-
-const pageSize = 10; // Лимит записей на странице
 
 const columns: ColumnsType<DataType> = [
   {
@@ -24,7 +22,7 @@ const columns: ColumnsType<DataType> = [
     dataIndex: 'age',
     key: 'age',
   },
-  {
+  { 
     title: 'Адрес',
     dataIndex: 'address',
     key: 'address',
@@ -41,372 +39,126 @@ const columns: ColumnsType<DataType> = [
   },
 ];
 
-const data: DataType[] = [ {
-  key: '1',
-  name: 'Ivan Ivanov',
-  age: 19,
-  address: 'ул. Прянишникова, 2А',
-  email: 'ivan228@mail.ru',
-  tags: ['Крутой'],
-},
-{
-  key: '2',
-  name: 'Ismatov Beka',
-  age: 19,
-  address: 'Михалковская улица, 7к3',
-  email: 'ulugb.ps@gmail.com',
-  tags: ['плохой'],
-},
-{
-  key: '3',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '4',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '5',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '6',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '7',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '8',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '9',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '10',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '11',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '12',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '13',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '14',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '1',
-  name: 'Ivan Ivanov',
-  age: 19,
-  address: 'ул. Прянишникова, 2А',
-  email: 'ivan228@mail.ru',
-  tags: ['Крутой'],
-},
-{
-  key: '2',
-  name: 'Ismatov Beka',
-  age: 19,
-  address: 'Михалковская улица, 7к3',
-  email: 'ulugb.ps@gmail.com',
-  tags: ['плохой'],
-},
-{
-  key: '3',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '4',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '5',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '6',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '7',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '8',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '9',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '10',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '11',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '12',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '13',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '14',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-}, {
-  key: '1',
-  name: 'Ivan Ivanov',
-  age: 19,
-  address: 'ул. Прянишникова, 2А',
-  email: 'ivan228@mail.ru',
-  tags: ['Крутой'],
-},
-{
-  key: '2',
-  name: 'Ismatov Beka',
-  age: 19,
-  address: 'Михалковская улица, 7к3',
-  email: 'ulugb.ps@gmail.com',
-  tags: ['плохой'],
-},
-{
-  key: '3',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '4',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '5',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '6',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '7',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '8',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '9',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '10',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '11',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '12',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '13',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-{
-  key: '14',
-  name: 'Maria Dovgiy',
-  age: 18,
-  address: 'Большая Семёновская ул., 38',
-  email: 'dovgiy12@mail.tj',
-  tags: ['анлак'],
-},
-]; 
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'Ivan Ivanov',
+    age: 19,
+    address: 'ул. Прянишникова, 2А',
+    email: 'ivan228@mail.ru',
+    tags: ['Крутой'],
+  },
+  {
+    key: '2',
+    name: 'Ismatov Beka',
+    age: 19,
+    address: 'Михалковская улица, 7к3',
+    email: 'ulugb.ps@gmail.com',
+    tags: ['плохой'],
+  },
+  {
+    key: '3',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '4',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '5',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '6',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '7',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '8',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '9',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '10',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '11',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '12',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '13',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+  {
+    key: '14',
+    name: 'Maria Dovgiy',
+    age: 18,
+    address: 'Большая Семёновская ул., 38',
+    email: 'dovgiy12@mail.tj',
+    tags: ['анлак'],
+  },
+];
 
 const App: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handleNextPage = () => {
-    setCurrentPage((prev) => prev + 1);
-  };
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prev) => prev - 1);
-    }
-  };
-
-  const startIndex = (currentPage - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-  const displayedData = data.slice(startIndex, endIndex);
-
   return (
     <div>
       <h1>Таблица</h1>
-      <Table columns={columns} dataSource={displayedData} />
-
-      <Button onClick={handlePrevPage} disabled={currentPage === 1}>
-        Назад
-      </Button>
-      <span style={{ margin: '0 10px' }}>Страница {currentPage}</span>
-      <Button onClick={handleNextPage}>
-        Вперёд
-      </Button>
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 };
